da4a07f12c53e3238238e4d3860a3925eafc7990
ozen-hub <info.ozenhub@gmail.com>
2024-01-26 15:36:40 +0530

    1

---
+++ index.js
@@ -0,0 +1,21 @@
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const port = process.env.SERVER_PORT;
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extend:false}));
+app.use(bodyParser.json());
+
+try{
+    mongoose.connect('mongodb://localhost:27017/utility_db');
+    app.listen(port,()=>{
+        console.log('server up and running!');
+    })
+}catch (e){
+    console.log(e)
+}
---
+++ model/cartSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const cartSchema= new mongoose.Schema(
+    {
+        date:{
+            type:Date,
+            required:true
+        },
+        qty:{
+            type:Number,
+            required:true
+        },
+        user:{
+            type:Number,
+            required:true
+        },
+        product:{
+            type:Number,
+            required:true
+        }
+    }
+);
+
+module.exports=mongoose.model('cart',cartSchema);
